f67dc87f061a6267c0bbe360e717fc5fef44d7d2
Ravi Singhal <ravi.r.singhal@oracle.com>
2020-08-12 16:14:33 +0530

    fix copyright

--- scripts/hooks/after_app_create.js
+++ scripts/hooks/after_app_create.js
@@ -1,9 +1,6 @@
-/**
-  Copyright (c) 2015, 2020, Oracle and/or its affiliates.
-  Licensed under The Universal Permissive License (UPL), Version 1.0
-  as shown at https://oss.oracle.com/licenses/upl/
+// Copyright (c) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-*/
 'use strict';
 
 module.exports = function () {
