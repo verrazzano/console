6037da4d1ea0f7051db7b30748e2882ff4c2fb46
Deva Sagar <deva.sagar@oracle.com>
2020-08-17 13:39:08 -0400

    Added /api server side proxying to the VZ API server

--- server.js
+++ server.js
@@ -3,12 +3,29 @@
 
 path = require('path');
 express = require('express');
+proxy = require('express-http-proxy');
 
-const port = 8183;
+const port = 8000;
 const app = express();
 const staticPath = path.join(__dirname, 'web');
+let apiUrl = process.env.VZ_API_URL;
+if (!apiUrl) {
+  if (process.env.VERRAZZANO_OPERATOR_SERVICE_HOST && process.env.VERRAZZANO_OPERATOR_SERVICE_PORT_API) {
+    apiUrl = "http://" + process.env.VERRAZZANO_OPERATOR_SERVICE_HOST + ":" + process.env.VERRAZZANO_OPERATOR_SERVICE_PORT_API;
+  } else {
+    throw "VZ_API_URL not specified and in-pod environment variables VERRAZZANO_OPERATOR_SERVICE_HOST/VERRAZZANO_OPERATOR_SERVICE_PORT_API not available. Aborting..";
+  }
+}
 
 app.use(express.static(staticPath));
+
+app.use('/api', proxy(apiUrl, {
+  proxyReqOptDecorator: function(proxyReqOpts, _) {
+    proxyReqOpts.rejectUnauthorized = false // Don't do 2-way SSL verification
+    return proxyReqOpts;
+  }
+}));
+
 app.listen(port, '0.0.0.0', function onStart(err) {
   if (err) {
     console.log(err);
