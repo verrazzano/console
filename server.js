a4629e032e1fe3e063c5af94562b318ee6a1b151
Deva Sagar <deva.sagar@oracle.com>
2020-08-18 10:40:24 -0400

    Create env.js correctly in both server.js and before_serve.js

--- server.js
+++ server.js
@@ -17,6 +17,40 @@ if (!apiUrl) {
   }
 }
 
+/**
+ * Uses environment variables specified at build time to generate an env.js file in the Javascript output directory
+ * The env.js file is sourced by index.html to make those values available to the application at runtime.
+ */
+function createEnvJs() {
+  const fs = require('fs');
+  const envJsFilePath = path.join(staticPath, 'js/env.js');
+  try {
+    fs.unlinkSync(envJsFilePath);
+    console.log(`Removed existing environment file ${envJsFilePath}`);
+  } catch (e) {
+    if (e.message.includes('ENOENT')) {
+      console.log(`No existing ${envJsFilePath} found`)
+    } else {
+      console.log(`Error deleting existing ${envJsFilePath}: ${e}`)
+      throw e
+    }
+  }
+
+  try {
+    console.log("Creating env.js.");
+    fs.writeFileSync(
+      `${envJsFilePath}`,
+      `var vzUiUrl = "${process.env.VZ_UI_URL}"; var vzKeycloakUrl = "${process.env.VZ_KEYCLOAK_URL}"; var vzAuth = "${process.env.VZ_AUTH || true}"; var vzClientId = "${process.env.VZ_CLIENT_ID}"; var vzApiUrl = "${apiUrl}"`,
+      { flag: 'wx' }
+    );
+    console.log(`${envJsFilePath} created.`);
+  } catch (e) {
+    console.log(`Failed creating ${envJsFilePath}: ${e}`);
+    throw e
+  }
+}
+
+createEnvJs();
 app.use(express.static(staticPath));
 
 app.use('/api', proxy(apiUrl, {
