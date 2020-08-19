c5704284058868953d94f8a51c0026774fdc4acb
Ravi Singhal <ravi.r.singhal@oracle.com>
2020-08-19 19:38:39 +0530

    no apiUrl in env.js when from server.js as we are proxying it

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ function createEnvJs() {
     console.log("Creating env.js.");
     fs.writeFileSync(
       `${envJsFilePath}`,
-      `var vzUiUrl = "${process.env.VZ_UI_URL}"; var vzKeycloakUrl = "${process.env.VZ_KEYCLOAK_URL}"; var vzAuth = "${process.env.VZ_AUTH || true}"; var vzClientId = "${process.env.VZ_CLIENT_ID}"; var vzApiUrl = "${apiUrl}"`,
+      `var vzUiUrl = "${process.env.VZ_UI_URL}"; var vzKeycloakUrl = "${process.env.VZ_KEYCLOAK_URL}"; var vzAuth = "${process.env.VZ_AUTH || true}"; var vzClientId = "${process.env.VZ_CLIENT_ID}"`,
       { flag: 'wx' }
     );
     console.log(`${envJsFilePath} created.`);
