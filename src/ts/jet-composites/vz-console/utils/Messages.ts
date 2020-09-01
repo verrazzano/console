2e060210546067e948d16eeb8da08f6f9149542f
Ravi Singhal <ravi.r.singhal@oracle.com>
2020-09-01 17:10:01 +0530

    fix copyright

--- src/ts/jet-composites/vz-console/utils/Messages.ts
+++ src/ts/jet-composites/vz-console/utils/Messages.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 import * as AppResource from 'ojL10n!./resources/nls/strings';
 import Translations = require('ojs/ojtranslation')
 import * as DefaultBundle from 'ojL10n!ojtranslations/nls/ojtranslations'
