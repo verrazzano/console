2970a3f72dcbbf650e1fcbbf9639aaf644d47421
Ravi Singhal <ravi.singhal.cse.21@gmail.com>
2020-10-23 09:04:24 +0530

    Fix binding vmi links in binding details page (#28)

---
+++ test/specs/binding/ConsoleBinding.spec.ts
@@ -0,0 +1,131 @@
+// Copyright (C) 2020, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+import {
+  Binding,
+  Instance,
+  VerrazzanoApi,
+  Secret,
+  getVmiInstancesForBinding,
+  VMIType,
+} from "vz-console/service/loader";
+import "vz-console/binding/loader";
+import * as Context from "ojs/ojcontext";
+import * as ko from "knockout";
+import "ojs/ojknockout";
+import * as sinon from "sinon";
+
+const expect = chai.expect;
+let bindingElement: HTMLElement;
+
+const instanceIp = "0.0.0.0";
+const instance = <Instance>{
+  id: "0",
+  elasticUrl: `https://elasticsearch.vmi.system.default.${instanceIp}.xip.io`,
+  kibanaUrl: `https://kibana.vmi.system.default.${instanceIp}.xip.io`,
+  grafanaUrl: `https://grafana.vmi.system.default.${instanceIp}.xip.io`,
+  prometheusUrl: `https://prometheus.vmi.system.default.${instanceIp}.xip.io`,
+};
+const bindings = {
+  test: <Binding>{
+    id: "test",
+    name: "test-binding",
+    description: "test",
+    model: { id: "test", modelComponents: [] },
+    components: [],
+    vmiInstances: getVmiInstancesForBinding("test-binding", instance),
+  },
+};
+const secrets: [Secret] = [
+  {
+    id: "test secret",
+    name: "test secret",
+    namespace: "test ns",
+    status: "",
+    type: "",
+  },
+];
+
+async function setup(bindingId: string) {
+  fixture.set(
+    `<div id ="binding-holder"><vz-console-binding id="binding" binding-id="[[bindingId]]"/></div>`
+  );
+  bindingElement = document.querySelector("#binding") as HTMLElement;
+  expect(bindingElement).not.to.be.null;
+  ko.applyBindings(
+    {
+      bindingId,
+    },
+    bindingElement
+  );
+  await Context.getContext(bindingElement)
+    .getBusyContext()
+    .whenReady(30000)
+    .catch((err) => {
+      console.log(err);
+    });
+}
+describe("binding vmi links test", () => {
+  const stubGetInstance = sinon.stub(
+    VerrazzanoApi.prototype,
+    <any>"getInstance"
+  );
+  const stubGetBinding = sinon.stub(VerrazzanoApi.prototype, <any>"getBinding");
+  const stubListSecrets = sinon.stub(
+    VerrazzanoApi.prototype,
+    <any>"listSecrets"
+  );
+  stubGetInstance.returns(Promise.resolve(instance));
+  stubGetBinding.returns(bindings.test);
+  stubListSecrets.returns(secrets);
+  before(async () => await setup("test"));
+
+  it("renders the binding vmi links correctly.", async () => {
+    const elasticSearchLink = bindingElement.querySelector(
+      `#binding-vmi-link-${VMIType.ElasticSearch.toLocaleLowerCase()} > * > a`
+    );
+    expect(elasticSearchLink).not.to.be.null;
+
+    const kibanaLink = bindingElement.querySelector(
+      `#binding-vmi-link-${VMIType.Kibana.toLocaleLowerCase()} > * > a`
+    );
+    expect(kibanaLink).not.to.be.null;
+
+    const grafanaLink = bindingElement.querySelector(
+      `#binding-vmi-link-${VMIType.Grafana.toLocaleLowerCase()} > * > a`
+    );
+    expect(grafanaLink).not.to.be.null;
+
+    const prometheusLink = bindingElement.querySelector(
+      `#binding-vmi-link-${VMIType.Prometheus.toLocaleLowerCase()} > * > a`
+    );
+    expect(prometheusLink).not.to.be.null;
+
+    const elasticSearchUrl = instance.elasticUrl.replace(
+      ".vmi.system.",
+      `.vmi.${bindings.test.name.toLowerCase()}.`
+    );
+    const kibanaUrl = instance.kibanaUrl.replace(
+      ".vmi.system.",
+      `.vmi.${bindings.test.name.toLowerCase()}.`
+    );
+    const grafanaUrl = instance.grafanaUrl.replace(
+      ".vmi.system.",
+      `.vmi.${bindings.test.name.toLowerCase()}.`
+    );
+    const prometheusUrl = instance.prometheusUrl.replace(
+      ".vmi.system.",
+      `.vmi.${bindings.test.name.toLowerCase()}.`
+    );
+
+    expect(elasticSearchLink.textContent).to.equal(elasticSearchUrl);
+    expect(elasticSearchLink.getAttribute("href")).to.equal(elasticSearchUrl);
+    expect(kibanaLink.textContent).to.equal(kibanaUrl);
+    expect(kibanaLink.getAttribute("href")).to.equal(kibanaUrl);
+    expect(grafanaLink.textContent).to.equal(grafanaUrl);
+    expect(grafanaLink.getAttribute("href")).to.equal(grafanaUrl);
+    expect(prometheusLink.textContent).to.equal(prometheusUrl);
+    expect(prometheusLink.getAttribute("href")).to.equal(prometheusUrl);
+  });
+  afterEach(() => fixture.cleanup());
+});
--- test/test-main.js
+++ test/test-main.js
@@ -1,8 +1,8 @@
 // Copyright (C) 2020, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-var allTestFiles = []
-var TEST_REGEXP = /(spec|test)\.js$/i
+var allTestFiles = [];
+var TEST_REGEXP = /(spec|test)\.js$/i;
 
 // Get a list of all the test files to include
 Object.keys(window.__karma__.files).forEach(function (file) {
@@ -10,44 +10,44 @@ Object.keys(window.__karma__.files).forEach(function (file) {
     // Normalize paths to RequireJS module names.
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '')
-    allTestFiles.push('../../' + normalizedTestModule)
+    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, "");
+    allTestFiles.push("../../" + normalizedTestModule);
   }
-})
+});
 
 function _ojIsIE11() {
   var nAgt = navigator.userAgent;
-  return nAgt.indexOf('MSIE') !== -1 || !!nAgt.match(/Trident.*rv:11./);
-};
+  return nAgt.indexOf("MSIE") !== -1 || !!nAgt.match(/Trident.*rv:11./);
+}
 var _ojNeedsES5 = _ojIsIE11();
 console.log("ALL TEST FILES");
 console.log(allTestFiles);
 
 require.config({
   // Karma serves files under /base, which is the basePath from your config file
-  baseUrl: '/base/web/js',
+  baseUrl: "/base/web/js",
 
   // Configure RequireJS path mappings
   // Note that every file listed here must also be listed in Karma's "files" array
   paths: {
-    "jquery":"libs/jquery/jquery-3.5.1",
-    "ojs":"libs/oj/current/debug" + (_ojNeedsES5 ? "_es5" : ""),
-    "ojL10n":"libs/oj/current/ojL10n",
-    "ojtranslations":"libs/oj/current/resources",
-    "sinon": "../../node_modules/sinon/pkg/sinon",
+    jquery: "libs/jquery/jquery-3.5.1",
+    ojs: "libs/oj/current/debug" + (_ojNeedsES5 ? "_es5" : ""),
+    ojL10n: "libs/oj/current/ojL10n",
+    ojtranslations: "libs/oj/current/resources",
+    sinon: "../../node_modules/sinon/pkg/sinon",
     "vz-console": "jet-composites/vz-console/1.0.0",
-    "knockout":"libs/knockout/knockout-3.5.1.debug",
-    "jqueryui-amd":"libs/jquery/jqueryui-amd-1.12.1",
-    "js-yaml":"libs/js-yaml/js-yaml.min",
-    "hammerjs":"libs/hammer/hammer-2.0.8",
-    "touchr":"libs/touchr/touchr",
-    "customElements":"libs/webcomponents/custom-elements.min",
-    "ojdnd":"libs/dnd-polyfill/dnd-polyfill-1.0.2"
+    knockout: "libs/knockout/knockout-3.5.1.debug",
+    "jqueryui-amd": "libs/jquery/jqueryui-amd-1.12.1",
+    "js-yaml": "libs/js-yaml/js-yaml.min",
+    hammerjs: "libs/hammer/hammer-2.0.8",
+    touchr: "libs/touchr/touchr",
+    customElements: "libs/webcomponents/custom-elements.min",
+    ojdnd: "libs/dnd-polyfill/dnd-polyfill-1.0.2",
   },
 
   // dynamically load all test files
   deps: allTestFiles,
 
   // we have to kickoff jasmine, as it is asynchronous
-  callback: window.__karma__.start
-})
+  callback: window.__karma__.start,
+});
