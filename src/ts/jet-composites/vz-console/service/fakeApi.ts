16072c3a1bc98351a8bdf0c3002ce456240b8e68
Ravi Singhal <ravi.r.singhal@oracle.com>
2021-01-05 16:57:52 +0530

    remove mocked data

--- src/ts/jet-composites/vz-console/service/fakeApi.ts
+++ src/ts/jet-composites/vz-console/service/fakeApi.ts
@@ -1,20 +1,41 @@
 // Copyright (c) 2020, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+// kubectl get applicationconfigurations --all-namespaces -o json > ./src/ts/jet-composites/vz-console/service/apps.json
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import * as allApps from "text!./apps.json";
+// import * as allApps from "text!./apps.json";
+
+// kubectl get components --all-namespaces -o json > ./src/ts/jet-composites/vz-console/service/components.json
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import * as allComponents from "text!./components.json";
+// import * as allComponents from "text!./components.json";
+
+// kubectl get containerizedworkloads --all-namespaces -o json > src/ts/jet-composites/vz-console/service/containerizedworkloads.json
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import * as allContainerizedWorkloads from "text!./containerizedworkloads.json";
+// import * as allContainerizedWorkloads from "text!./containerizedworkloads.json";
+
+// kubectl get deployments --all-namespaces -o json > src/ts/jet-composites/vz-console/service/deployments.json
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import * as allDeployments from "text!./deployments.json";
+// import * as allDeployments from "text!./deployments.json";
+
+// kubectl get domains --all-namespaces -o json > src/ts/jet-composites/vz-console/service/domains.json
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import * as allDomains from "text!./domains.json";
+// import * as allDomains from "text!./domains.json";
+
+// kubectl get healthscopes --all-namespaces -o json > src/ts/jet-composites/vz-console/service/healthscopes.json
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import * as allHealthScopes from "text!./healthscopes.json";
+// import * as allHealthScopes from "text!./healthscopes.json";
+
+// kubectl get ingresstraits --all-namespaces -o json > src/ts/jet-composites/vz-console/service/ingresstraits.json
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import * as allIngressTraits from "text!./ingresstraits.json";
+// import * as allIngressTraits from "text!./ingresstraits.json";
+
+const allApps = '{"items": []}';
+const allComponents = '{"items": []}';
+const allContainerizedWorkloads = '{"items": []}';
+const allDeployments = '{"items": []}';
+const allDomains = '{"items": []}';
+const allHealthScopes = '{"items": []}';
+const allIngressTraits = '{"items": []}';
 
 export const getOamApplications = (): string => {
   return JSON.stringify(JSON.parse(allApps).items);
