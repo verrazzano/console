5d1951d826bc46666f96e295c285f040f089ef33
Mike Cico <michael.cico@oracle.com>
2021-03-26 15:31:20 -0400

    Update unit tests for new field

--- test/specs/instance/ConsoleInstance.spec.ts
+++ test/specs/instance/ConsoleInstance.spec.ts
@@ -30,6 +30,7 @@ const instance = <Instance>{
   mgmtCluster: "test",
   version: "1.0",
   status: "OK",
+  profile: "Production",
 };
 const sandbox = sinon.createSandbox();
 
@@ -154,6 +155,11 @@ describe("instance panel screen tests", () => {
     );
     expect(rancherMetaItem).not.to.be.null;
 
+    const profileMetaItem = instanceElement.querySelector(
+      `#instance-profile-metaitem`
+    );
+    expect(profileMetaItem).not.to.be.null;
+
     checkMetaItemLabelValue(
       statusMetaItem.textContent,
       Messages.Labels.status(),
@@ -178,6 +184,11 @@ describe("instance panel screen tests", () => {
       rancherMetaItem.textContent,
       Messages.Labels.rancher(),
       instance.rancherUrl
+    );
+    checkMetaItemLabelValue(
+      profileMetaItem.textContent,
+      Messages.Labels.profile(),
+      instance.profile
     );
     expect(
       keycloakMetaItem.querySelector("* > a").getAttribute("href")
