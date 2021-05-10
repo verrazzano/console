8c1f1b7a1ee017af1b1cb2a411dcb0b5877e5e85
Mason Taube <mason.taube@oracle.com>
2021-05-10 17:19:33 -0400

    lint

--- src/ts/jet-composites/vz-console/instance-projects/instance-projects.tsx
+++ src/ts/jet-composites/vz-console/instance-projects/instance-projects.tsx
@@ -1,10 +1,10 @@
 // Copyright (c) 2021, Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-// eslint-disable-next-line no-unused-vars
 import {
   ElementVComponent,
   customElement,
+  // eslint-disable-next-line no-unused-vars
   h,
   listener,
 } from "ojs/ojvcomponent-element";
