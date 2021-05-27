2f48702a70133104256c828cd365159b33cca58a
Mason Taube <mason.taube@oracle.com>
2021-05-27 13:49:18 -0400

    fix spacing

--- src/ts/jet-composites/vz-console/oamapp-component-view/oamapp-component-view.tsx
+++ src/ts/jet-composites/vz-console/oamapp-component-view/oamapp-component-view.tsx
@@ -40,122 +40,138 @@ export class ConsoleOAMAppComponentView extends ElementVComponent<Props> {
           <oj-list-item-layout>
             <div class="oj-flex">
               <div class="oj-sm-10 oj-flex-item">
-                <strong>
-                  <span>{Messages.Labels.name()}:&nbsp;</span>
-                </strong>
-                <span data-bind="attr: { id: item.data.id+'_name' }">
-                  <oj-bind-text value="[[item.data.name]]"></oj-bind-text>
-                </span>
+                <div class="carditem">
+                  <strong>
+                    <span>{Messages.Labels.name()}:&nbsp;</span>
+                  </strong>
+                  <span data-bind="attr: { id: item.data.id+'_name' }">
+                    <oj-bind-text value="[[item.data.name]]"></oj-bind-text>
+                  </span>
+                </div>
               </div>
               <div class="oj-sm-2 oj-flex-item">
-                <a
-                  data-bind="event: { click: () => {item.data.eventHandler('traits', item.data.id)}, keypress: () => {item.data.eventHandler('traits', item.data.id)} }"
-                  tabindex="0"
-                >
-                  {Messages.ComponentConfigLabels.traits}
-                </a>
+                <div class="carditem">
+                  <a
+                    data-bind="event: { click: () => {item.data.eventHandler('traits', item.data.id)}, keypress: () => {item.data.eventHandler('traits', item.data.id)} }"
+                    tabindex="0"
+                  >
+                    {Messages.ComponentConfigLabels.traits}
+                  </a>
+                </div>
               </div>
             </div>
             <div class="oj-flex">
               <div class="oj-sm-10 oj-flex-item compstatus">
-                <strong>{Messages.Labels.status()}:&nbsp;</strong>
-                <span data-bind="attr: { id: item.data.id+'_status' }">
-                  <oj-bind-if test="[[item.data.status === 'Running']]">
-                    <span class="oj-icon-circle oj-icon-circle-sm oj-icon-circle-green">
-                      <span class="oj-icon-circle-inner status-icon"></span>
-                    </span>
-                  </oj-bind-if>
-                  <oj-bind-if test="[[item.data.status === 'Terminated']]">
-                    <span class="oj-icon-circle oj-icon-circle-sm oj-icon-circle-red">
-                      <span class="oj-icon-circle-inner status-icon"></span>
-                    </span>
-                  </oj-bind-if>
-                  <oj-bind-if test="[[item.data.status === 'Pending']]">
-                    <span class="oj-icon-circle oj-icon-circle-sm oj-icon-circle-orange">
-                      <span class="oj-icon-circle-inner status-icon"></span>
-                    </span>
-                  </oj-bind-if>
-                  &nbsp;
-                  <oj-bind-text value="[[item.data.status]]"></oj-bind-text>
-                </span>
+                <div class="carditem">
+                  <strong>{Messages.Labels.status()}:&nbsp;</strong>
+                  <span data-bind="attr: { id: item.data.id+'_status' }">
+                    <oj-bind-if test="[[item.data.status === 'Running']]">
+                      <span class="oj-icon-circle oj-icon-circle-sm oj-icon-circle-green">
+                        <span class="oj-icon-circle-inner status-icon"></span>
+                      </span>
+                    </oj-bind-if>
+                    <oj-bind-if test="[[item.data.status === 'Terminated']]">
+                      <span class="oj-icon-circle oj-icon-circle-sm oj-icon-circle-red">
+                        <span class="oj-icon-circle-inner status-icon"></span>
+                      </span>
+                    </oj-bind-if>
+                    <oj-bind-if test="[[item.data.status === 'Pending']]">
+                      <span class="oj-icon-circle oj-icon-circle-sm oj-icon-circle-orange">
+                        <span class="oj-icon-circle-inner status-icon"></span>
+                      </span>
+                    </oj-bind-if>
+                    &nbsp;
+                    <oj-bind-text value="[[item.data.status]]"></oj-bind-text>
+                  </span>
+                </div>
               </div>
               <div class="oj-sm-2 oj-flex-item">
-                <a
-                  data-bind="event: { click: () => {item.data.eventHandler('scopes', item.data.id)}, keypress: () => {item.data.eventHandler('scopes', item.data.id)} }"
-                  tabindex="0"
-                >
-                  {Messages.ComponentConfigLabels.scopes}
-                </a>
+                <div class="carditem">
+                  <a
+                    data-bind="event: { click: () => {item.data.eventHandler('scopes', item.data.id)}, keypress: () => {item.data.eventHandler('scopes', item.data.id)} }"
+                    tabindex="0"
+                  >
+                    {Messages.ComponentConfigLabels.scopes}
+                  </a>
+                </div>
               </div>
             </div>
             <div class="oj-flex">
               <div class="oj-sm-10 oj-flex-item">
-                <strong>
-                  <span>{Messages.Labels.created()}:&nbsp;</span>
-                </strong>
-                <span data-bind="attr: { id: item.data.id+'_created' }">
-                  <oj-bind-text value="[[item.data.creationDate]]"></oj-bind-text>
-                </span>
+                <div class="carditem">
+                  <strong>
+                    <span>{Messages.Labels.created()}:&nbsp;</span>
+                  </strong>
+                  <span data-bind="attr: { id: item.data.id+'_created' }">
+                    <oj-bind-text value="[[item.data.creationDate]]"></oj-bind-text>
+                  </span>
+                </div>
               </div>
               <div class="oj-sm-2 oj-flex-item">
-                <a
-                  data-bind="event: { click: () => {item.data.eventHandler('params', item.data.id)}, keypress: () => {item.data.eventHandler('params', item.data.id)} }"
-                  tabindex="0"
-                >
-                  {Messages.ComponentConfigLabels.params}
-                </a>
+                <div class="carditem">
+                  <a
+                    data-bind="event: { click: () => {item.data.eventHandler('params', item.data.id)}, keypress: () => {item.data.eventHandler('params', item.data.id)} }"
+                    tabindex="0"
+                  >
+                    {Messages.ComponentConfigLabels.params}
+                  </a>
+                </div>
               </div>
             </div>
             <div class="oj-flex">
               <div class="oj-sm-12 oj-flex-item">
-                <strong>
-                  <span>{Messages.Labels.oamCompRef()}:&nbsp;</span>
-                </strong>
-                <a
-                  data-bind={`attr: {href: '/oamcomps/' + item.data.oamComponent.data.metadata.uid + (item.data.oamComponent.cluster && item.data.oamComponent.cluster.name !== 'local' ? ('?cluster=' + item.data.oamComponent.cluster.name) : '')}`}
-                  tabindex="0"
-                >
-                  <oj-bind-text value="[[item.data.oamComponent.data.metadata.name]]"></oj-bind-text>
-                </a>
+                <div class="carditem">
+                  <strong>
+                    <span>{Messages.Labels.oamCompRef()}:&nbsp;</span>
+                  </strong>
+                  <a
+                    data-bind={`attr: {href: '/oamcomps/' + item.data.oamComponent.data.metadata.uid + (item.data.oamComponent.cluster && item.data.oamComponent.cluster.name !== 'local' ? ('?cluster=' + item.data.oamComponent.cluster.name) : '')}`}
+                    tabindex="0"
+                  >
+                    <oj-bind-text value="[[item.data.oamComponent.data.metadata.name]]"></oj-bind-text>
+                  </a>
+                </div>
               </div>
             </div>
             <div class="oj-flex">
               <div class="oj-sm-12 oj-flex-item">
-                <strong>
-                  <span>{Messages.Labels.workload()}:&nbsp;</span>
-                </strong>
-                <a
-                  data-bind="event: { click: () => {item.data.workloadOpenEventHandler()} }, attr: {id: 'workload_' + item.data.id}"
-                  tabindex="0"
-                >
-                  <oj-bind-text value="[[item.data.oamComponent.workloadType]]"></oj-bind-text>
-                </a>
-                <oj-popup
-                  data-bind={`attr: {id: 'popup_' + item.data.id}`}
-                  modality="modal"
-                  {...{ "position.my.horizontal": "center" }}
-                  {...{ "position.my.vertical": "bottom" }}
-                  {...{ "position.at.horizontal": "center" }}
-                  {...{ "position.at.vertical": "bottom" }}
-                  {...{ "position.offset.y": "-10" }}
-                  tail="none"
-                  class="popup"
-                >
-                  <div class="popupbody">
-                    <div>
-                      <a
-                        data-bind="event: { click: () => {item.data.workloadCloseEventHandler()} }"
-                        class="closelink"
-                        tabindex="0"
-                      >
-                        Close
-                      </a>
+                <div class="carditem">
+                  <strong>
+                    <span>{Messages.Labels.workload()}:&nbsp;</span>
+                  </strong>
+                  <a
+                    data-bind="event: { click: () => {item.data.workloadOpenEventHandler()} }, attr: {id: 'workload_' + item.data.id}"
+                    tabindex="0"
+                  >
+                    <oj-bind-text value="[[item.data.oamComponent.workloadType]]"></oj-bind-text>
+                  </a>
+                  <oj-popup
+                    data-bind={`attr: {id: 'popup_' + item.data.id}`}
+                    modality="modal"
+                    {...{ "position.my.horizontal": "center" }}
+                    {...{ "position.my.vertical": "bottom" }}
+                    {...{ "position.at.horizontal": "center" }}
+                    {...{ "position.at.vertical": "bottom" }}
+                    {...{ "position.offset.y": "-10" }}
+                    tail="none"
+                    class="popup"
+                  >
+                    <div class="popupbody">
+                      <div>
+                        <a
+                          data-bind="event: { click: () => {item.data.workloadCloseEventHandler()} }"
+                          class="closelink"
+                          tabindex="0"
+                        >
+                          Close
+                        </a>
+                      </div>
+                      <pre class="popupcontent">
+                        <oj-bind-text value="[[item.data.descriptor]]"></oj-bind-text>
+                      </pre>
                     </div>
-                    <pre class="popupcontent">
-                      <oj-bind-text value="[[item.data.descriptor]]"></oj-bind-text>
-                    </pre>
-                  </div>
-                </oj-popup>
+                  </oj-popup>
+                </div>
               </div>
             </div>
           </oj-list-item-layout>
