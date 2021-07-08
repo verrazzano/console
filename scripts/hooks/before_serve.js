b368c5bdcbdda84458c30c44690dd0f961002873
kate.nelson <kate.nelson@oracle.com>
2021-07-08 15:27:00 -0700

    ran prettier-write and eslint post rebase

--- scripts/hooks/before_serve.js
+++ scripts/hooks/before_serve.js
@@ -26,10 +26,14 @@ function createEnvJs() {
     console.log("Creating env.js.");
     fs.writeFileSync(
       `${envJsFilePath}`,
-      `var vzUiUrl = "${process.env.VZ_UI_URL}"; var vzKeycloakUrl = "${process.env.VZ_KEYCLOAK_URL
-      }"; var vzAuth = "${process.env.VZ_AUTH || true}"; var vzClientId = "${process.env.VZ_CLIENT_ID
-      }"; var vzApiUrl = "${process.env.VZ_API_URL || ""
-      }"; var vzWLSImagesEnabled = ${process.env.VZ_WLS_IMAGES_ENABLED || false
+      `var vzUiUrl = "${process.env.VZ_UI_URL}"; var vzKeycloakUrl = "${
+        process.env.VZ_KEYCLOAK_URL
+      }"; var vzAuth = "${process.env.VZ_AUTH || true}"; var vzClientId = "${
+        process.env.VZ_CLIENT_ID
+      }"; var vzApiUrl = "${
+        process.env.VZ_API_URL || ""
+      }"; var vzWLSImagesEnabled = ${
+        process.env.VZ_WLS_IMAGES_ENABLED || false
       };`,
       { flag: "wx" }
     );
@@ -45,13 +49,15 @@ function rewriteUrls() {
   const app = express();
   app.get("/oamapps", (req, res, next) => {
     res.redirect(
-      `/?ojr=instance&selectedItem=oamapps${req.query.cluster ? "&cluster=" + req.query.cluster : ""
+      `/?ojr=instance&selectedItem=oamapps${
+        req.query.cluster ? "&cluster=" + req.query.cluster : ""
       }`
     );
   });
   app.get("/oamcomps", (req, res, next) => {
     res.redirect(
-      `/?ojr=instance&selectedItem=oamcomps${req.query.cluster ? "&cluster=" + req.query.cluster : ""
+      `/?ojr=instance&selectedItem=oamcomps${
+        req.query.cluster ? "&cluster=" + req.query.cluster : ""
       }`
     );
   });
@@ -60,19 +66,22 @@ function rewriteUrls() {
   });
   app.get("/weblogicimages", (req, res, next) => {
     res.redirect(
-      `/?ojr=instance&selectedItem=weblogicimages${req.query.cluster ? "&cluster=" + req.query.cluster : ""
+      `/?ojr=instance&selectedItem=weblogicimages${
+        req.query.cluster ? "&cluster=" + req.query.cluster : ""
       }`
     );
   });
   app.get("/oamapps/:id", (req, res, next) => {
     res.redirect(
-      `/?ojr=oamapp&oamAppId=${req.params.id}${req.query.cluster ? "&cluster=" + req.query.cluster : ""
+      `/?ojr=oamapp&oamAppId=${req.params.id}${
+        req.query.cluster ? "&cluster=" + req.query.cluster : ""
       }`
     );
   });
   app.get("/oamcomps/:id", (req, res, next) => {
     res.redirect(
-      `/?ojr=oamcomp&oamCompId=${req.params.id}${req.query.cluster ? "&cluster=" + req.query.cluster : ""
+      `/?ojr=oamcomp&oamCompId=${req.params.id}${
+        req.query.cluster ? "&cluster=" + req.query.cluster : ""
       }`
     );
   });
@@ -81,14 +90,17 @@ function rewriteUrls() {
   });
   app.get("/oamapps/:id/components", (req, res, next) => {
     res.redirect(
-      `/?ojr=oamapp&oamAppId=${req.params.id}&selectedItem=components${req.query.cluster ? "&cluster=" + req.query.cluster : ""
+      `/?ojr=oamapp&oamAppId=${req.params.id}&selectedItem=components${
+        req.query.cluster ? "&cluster=" + req.query.cluster : ""
       }`
     );
   });
   app.get("/oamapps/:id/components/:selectedComponent", (req, res, next) => {
     res.redirect(
-      `/?ojr=oamapp&oamAppId=${req.params.id
-      }&selectedItem=components&selectedComponent=${req.params.selectedComponent
+      `/?ojr=oamapp&oamAppId=${
+        req.params.id
+      }&selectedItem=components&selectedComponent=${
+        req.params.selectedComponent
       }${req.query.cluster ? "&cluster=" + req.query.cluster : ""}`
     );
   });
@@ -96,15 +108,18 @@ function rewriteUrls() {
     "/oamapps/:id/components/:selectedComponent/:selectedItem",
     (req, res, next) => {
       res.redirect(
-        `/?ojr=oamapp&oamAppId=${req.params.id}&selectedItem=${req.params.selectedItem
-        }&selectedComponent=${req.params.selectedComponent}${req.query.cluster ? "&cluster=" + req.query.cluster : ""
+        `/?ojr=oamapp&oamAppId=${req.params.id}&selectedItem=${
+          req.params.selectedItem
+        }&selectedComponent=${req.params.selectedComponent}${
+          req.query.cluster ? "&cluster=" + req.query.cluster : ""
         }`
       );
     }
   );
   app.get("/oamcomps/:id/:selectedItem", (req, res, next) => {
     res.redirect(
-      `/?ojr=oamcomp&oamCompId=${req.params.id}&selectedItem=${req.params.selectedItem
+      `/?ojr=oamcomp&oamCompId=${req.params.id}&selectedItem=${
+        req.params.selectedItem
       }${req.query.cluster ? "&cluster=" + req.query.cluster : ""}`
     );
   });
