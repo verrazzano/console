c31c629c53e01a6acbba71b9ae6724e9b9df17dd
Ravi Singhal <ravi.r.singhal@oracle.com>
2021-07-27 12:55:00 +0530

    update main js and require js integrity value

--- start.sh
+++ start.sh
@@ -6,5 +6,7 @@
 set -x
 node generate-env.js
 ENV_JS_HASH=$(cat web/js/env.js | openssl dgst -sha384 -binary | openssl base64 -A)
-sed "s;ENV_JS_INTEGRITY_VALUE;${ENV_JS_HASH};g" -i web/index.html
+REQUIRE_JS_HASH=$(cat web/js/libs/require/require.js | openssl dgst -sha384 -binary | openssl base64 -A)
+MAIN_JS_HASH=$(cat web/js/main.js | openssl dgst -sha384 -binary | openssl base64 -A)
+sed -e "s;ENV_JS_INTEGRITY_VALUE;${ENV_JS_HASH};g" -e "s;REQUIRE_JS_INTEGRITY_VALUE;${REQUIRE_JS_HASH};g" -e "s;MAIN_JS_INTEGRITY_VALUE;${MAIN_JS_HASH};g" -i web/index.html
 node server.js
