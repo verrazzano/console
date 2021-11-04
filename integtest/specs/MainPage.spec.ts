620897bc0522fe247955034348aecfeda10ad463
anders-swanson <91502735+anders-swanson@users.noreply.github.com>
2021-11-04 12:03:24 -0700

    Anders/vz 3274 (#135)
    * VZ 3274: Kiali link on console
    * Disable Kiali tests

--- integtest/specs/MainPage.spec.ts
+++ integtest/specs/MainPage.spec.ts
@@ -105,7 +105,7 @@ describe("UI Tests for Home Pages (Console, Grafana, Kibana, Prometheus, Kiali)"
     });
   });
 
-  describe("Navigate to Kiali home page", (): void => {
+  xdescribe("Navigate to Kiali home page", (): void => {
     it("Wait for navigation to Kiali", async () => {
       await consoleMainPage.navigateToVMI("kiali", 4);
     });
