e2dbdcd6ec82d558abee7659485d699661663bd6
Deva Sagar <deva.sagar@oracle.com>
2022-05-19 11:09:00 -0400

    VZ-5817 Integration tests capture browser console logs on failure. (#195)

--- integtest/specs/PrometheusMainPage.spec.ts
+++ integtest/specs/PrometheusMainPage.spec.ts
@@ -43,8 +43,7 @@ describe("Prometheus Home Page", (): void => {
 
   afterEach(async function () {
     if (this.currentTest.state === "failed") {
-      const titleNoSpaces = this.currentTest.title.split(" ").join("_");
-      await Utils.takeScreenshot(`Screenshot_${titleNoSpaces}.png`);
+      await Utils.saveFailedTestInfo("Prometheus", this.currentTest.title);
     }
   });
 
