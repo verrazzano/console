d3cafd46771409aa949d0877795560b374c6b1b3
abnadago <abijitha.nadagouda@oracle.com>
2022-07-20 21:21:02 +0000

    Added to tests

--- integtest/specs/ApplicationPage.spec.ts
+++ integtest/specs/ApplicationPage.spec.ts
@@ -19,7 +19,7 @@ describe("Application Details Page", (): void => {
       );
     }
     console.log(`Testing application details for app ${appConfig.name}`);
-    const linkBy = By.id(`appname-${appConfig.name}`);
+    const linkBy = By.id(`appname-${appConfig.name}-ns-${appConfig.namespace}`);
     await Actions.scrollIntoView(linkBy);
     // await Actions.doClick(linkBy);
     try {
