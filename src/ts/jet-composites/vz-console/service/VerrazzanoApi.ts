c1cf0c596b35d31f674c38e8b6d9493325be846b
SNEHIL KHAJURIA <snehil.k.khajuria@oracle.com>
2022-10-21 16:15:47 +0530

    Changes

--- src/ts/jet-composites/vz-console/service/VerrazzanoApi.ts
+++ src/ts/jet-composites/vz-console/service/VerrazzanoApi.ts
@@ -351,7 +351,7 @@ export class VerrazzanoApi {
     name?: string
   ): Promise<Response> {
     for (let i = 0; i < 5; i++) {
-      console.log("This is" + i +"attempt");
+      console.log("This is" + i + "attempt");
       return Promise.resolve(
         this.fetchApi(
           `${this.url}/${type.ApiVersion}/${
